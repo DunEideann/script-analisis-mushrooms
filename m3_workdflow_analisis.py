273cc27e12efe7fd2bcf8ce31c0a9964bb18f060
Samuel Reyes <samuel.rys@gmail.com>
2024-01-30 17:37:33 +0100

    analisis change

--- m3_workdflow_analisis.py
+++ m3_workdflow_analisis.py
@@ -2,15 +2,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import export_text
 
 df = pd.read_csv("mushrooms.csv", encoding = "ISO-8859-1")
 
 # Separar la variable objetivo (class) y las características
-X = data.drop('class', axis=1)
-Y = data['class']
+X = df.drop('class', axis=1)
+Y = df['class']
+X_encoded = pd.get_dummies(X)
 
 #Dividir el conjunto de datos en conjuntos de entrenamiento y prueba (75% train, 25% test)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=11)
+X_train, X_test, Y_train, Y_test = train_test_split(X_encoded, Y, test_size=0.25, random_state=11)
 
 # Crear un modelo de árbol de clasificación
 clf = DecisionTreeClassifier(random_state=11)
@@ -26,6 +29,16 @@ accuracy = accuracy_score(Y_test, Y_pred)
 print(f'Precisión del modelo: {accuracy:.2f}')
 
 # Visualizar el árbol de clasificación si lo deseas
-from sklearn.tree import export_text
-tree_rules = export_text(clf, feature_names=X.columns.tolist())
-print(tree_rules)
+# Get the feature names after one-hot encoding
+feature_names_encoded = X_encoded.columns.tolist()
+
+
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
+
+# Create a figure and axis
+plt.figure(figsize=(20, 10))
+
+# Visualize the decision tree
+plot_tree(clf, filled=True, feature_names=feature_names_encoded, class_names=clf.classes_)
+plt.show()
